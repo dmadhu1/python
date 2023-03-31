b5e099ebdd93304551300630f143589e8b223ff2
dmadhu1 <129514445+dmadhu1@users.noreply.github.com>
2023-03-31 18:02:32 +0530

    2nd commit

---
+++ list.py
@@ -0,0 +1,15 @@
+# my_list1= [1,2,3,4]
+# my_list2= [2,4,6,8,10]
+
+# result = []  #empty list
+
+# for i in range(len(my_list2)):
+#     result.append(my_list1[i]*my_list2[i])
+
+#print(result)
+myList=[10,20,30,"Apple",True,8.10]
+myList.reverse()
+reverseList = [i for i in myList]
+print(reverseList)
+
+
